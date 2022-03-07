5667acfaac30196fd0a2df707fe8fc175fde3ece
sameer alam <alamsameer738@gmail.com>
2022-03-07 04:08:26 +0000

    completed mongo

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -11,17 +11,33 @@ app.use(json())
 
 app.get('/todo/:id', async (req, res) => {
   const todoId = req.params.id
+   try{
+    const todoList=await Todo.findById(todoId).lean()
+   
+    res.status(200).json(todoList)
+   }
+   catch{
+     res.status(501).send()
+   }
 })
-
 app.get('/todos', async (req, res) => {
-
+  const todoList=await Todo.find({}).lean()
+  res.json(todoList)
 })
 
 app.post('/todo', async () => {
   const todoToCreate = req.body.todo
+  try{
+const todo =await Todo.create(todoToCreate)
+res.status(201).send(todo.toObject())
+  
+  }
+  catch{
+    res.status(501).send()
+  }
 })
 
-connect(/**add mongo url here */)
+connect('mongodb://127.0.0.1:27017/notes')
   .then(() => app.listen(4000, () => {
     console.log('server on http://localhost:4000')
   }))
